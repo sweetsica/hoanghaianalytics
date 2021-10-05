f069ccd97242867e9213d29fbf4ba06c9068b15c
sweetsica <barondeepsnow@gmail.com>
2021-10-05 19:15:02 +0700

    update visitor mutators

--- app/Models/Visitor.php
+++ app/Models/Visitor.php
@@ -14,8 +14,77 @@ class Visitor extends Model
         return $this->hasMany(ShortLink::class,'link','visitor_clicked');
     }
 
-//    public function setVisitorAttribute($value)
-//    {
-//
-//    }
+//VisitorPlatform
+    public function setVisitorPlatformAttribute($value)
+    {
+        switch ($value){
+            case "iOS":
+                $this->attributes['visitor_clicked'] = 1;
+                break;
+            case "AndroidOS":
+                $this->attributes['visitor_clicked'] = 2;
+                break;
+            case "Linux":
+                $this->attributes['visitor_clicked'] = 3;
+                break;
+            case "Windows":
+                $this->attributes['visitor_clicked'] = 4;
+                break;
+            case "OS X":
+                $this->attributes['visitor_clicked'] = 5;
+                break;
+            default:
+                return $value;
+        }
+    }
+
+    public function getVisitorPlatformAttribute($value)
+    {
+        switch ($value){
+            case 1:
+                return "iOS";
+            case 2:
+                return "AndroidOS";
+            case 3:
+                return "Linux";
+            case 4:
+                return "Windows";
+            case 5:
+                return "OS X";
+            default:
+                return $value;
+        }
+    }
+
+//VisitorLinkfrom
+    public function setVisitorLinkfromAttribute($value)
+    {
+        switch ($value){
+            case "http://muahang.hoanghaimobile.jp":
+                $this->attributes['visitor_clicked'] = 1;
+                break;
+            case "https://www.tiktok.com":
+                $this->attributes['visitor_clicked'] = 2;
+                break;
+            case "https://hoanghaimobile.jp/mua-hang-tai-day?utm-source=sieuthidienthoai&group=21&ads=8":
+                $this->attributes['visitor_clicked'] = 3;
+                break;
+            default:
+                return $value;
+        }
+    }
+
+    public function getVisitorLinkfromAttribute($value)
+    {
+        switch ($value){
+            case 1:
+                return "http://muahang.hoanghaimobile.jp";
+            case 2:
+                return "https://www.tiktok.com";
+            case 3:
+                return "https://hoanghaimobile.jp/mua-hang-tai-day?utm-source=sieuthidienthoai&group=21&ads=8";
+            default:
+                return $value;
+        }
+    }
 }
